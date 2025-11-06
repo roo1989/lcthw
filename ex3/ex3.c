e51120af59acfcc531a4e6289bd73999d8ded07a
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-11-06 14:25:36 +0000

    ex3

---
+++ ex3/ex3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int age = 10;
+	int height = 72;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+
+	char first_letter = 'R';
+	printf("The first character of my name is: %i\n", first_letter);
+
+	return 0;
+}
